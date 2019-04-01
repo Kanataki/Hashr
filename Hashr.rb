35d3e3e8de39cd8dd1ed1ffbe159119820f20f1f
Kanataki <gichiniwaruingi@gmail.com>
2019-04-01 21:37:17 +0300

    added extension check and put converter in method

--- Hashr.rb
+++ Hashr.rb
@@ -1,46 +1,23 @@
+#load requirements
 require 'active_support/core_ext/hash'
-require 'rspec'
+#prompt user
 STDOUT.sync = true
 puts "Hashr Needs some xmls to convert"
 puts "what is your filename"
-file_name = STDIN.gets.chomp
-puts file_name
-extension = File.extname(file_name)
+#get filename from user
+$file_name = STDIN.gets.chomp
+#get extension of given file name
+extension = File.extname($file_name)
+#check enterd file extension
 if extension == '.xml'
 	puts "You have loaded a "+extension+" file"
+	def converter
+		file_data = File.open($file_name)
+		hashr = Hash.from_xml(file_data)
+		puts hashr
+	end
+	puts converter	
 else 
 	puts "the extension "+extension+" is not a supported format"
 end
-
-file_data = File.open(file_name)
-puts file_data
-hashr = Hash.from_xml(file_data)
-puts hashr
-
-#def macbeth_counter
-
-	#file_data = File.open(file)
-	#hash = Hash.from_xml(file_data)
-	#counter_hash = Hash.new{ |p, g| p[g] = 0}
-
-	#hash['PLAY']['ACT'].each do |g, v|
-		#g['SCENE'].each do |g2, v2|
-			#g2['SPEECH'].each do |g3, v3|
-				#counter_hash[g3['SPEAKER']] += Array(g3['LINE']).count
-			#end
-		#end
-	#end
-	#counter_hash
-#end
-
-#puts macbeth_counter
-
-
-#puts "--------------------------------------------------------------------"
-#
-#puts "Enter name of file: "
-#file = gets.chomp
-#puts file
-
-
 
